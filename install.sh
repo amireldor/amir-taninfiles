cbe08200ac7e8f4d1d09fb77f39e526b132fd082
Amir Eldor <amir.eldor@gmail.com>
2015-10-28 16:07:49 +0200

    Added messages to user in install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@
 
 FILES_DIR="thefiles"
 
+echo "This will create a symlink to all the interesting files in the interesting folder on your home folder."
+
 read -p "Go for it? (Y/n): " YESNO
 
 YESNO=${YESNO:-Y}
@@ -15,6 +17,13 @@ if [[ $YESNO =~ ^[yY]$ ]]; then
 	for f in $(ls $FILES_DIR); do
 		ln -vfs "$PWD/$FILES_DIR/$f" "$HOME/.$f"
 	done
+
+    echo
+    echo "You may with to run :VundleInstall on Vim and such."
+    echo "If your username is not amir and I still havent fixed this, note that"
+    echo "in .gitconfig, the 'gitexcludes' variable is hardcoded with username 'amir'"
+    echo
+
 fi
 
 
