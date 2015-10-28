cadff74da054f1c5f91c67e3c09fa12d9e7888a6
Amir Eldor <amir.eldor@gmail.com>
2015-10-28 16:14:04 +0200

    Re-indent of install.sh

--- install.sh
+++ install.sh
@@ -13,11 +13,11 @@ read -p "Go for it? (Y/n): " YESNO
 YESNO=${YESNO:-Y}
 
 if [[ $YESNO =~ ^[yY]$ ]]; then
-	echo "doing it..."
-	for f in $(ls $FILES_DIR); do
+    echo "doing it..."
+    for f in $(ls $FILES_DIR); do
         rm "$HOME/.$f"
-		ln -vs "$PWD/$FILES_DIR/$f" "$HOME/.$f"
-	done
+        ln -vs "$PWD/$FILES_DIR/$f" "$HOME/.$f"
+    done
 
     echo
     echo "You may with to run :VundleInstall on Vim and such."
