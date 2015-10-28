27e41d87e006a40541ad9ce9f90f82d55a9c2a70
Amir Eldor <amir.eldor@gmail.com>
2015-10-28 16:01:32 +0200

    install.sh doing its magic, at least for now

--- install.sh
+++ install.sh
@@ -4,18 +4,17 @@
 # well, it will also prepend a '.' to the filename so they are hidden and niceable.
 # prompts if to begin bixlal.
 
+FILES_DIR="thefiles"
+
 read -p "Go for it? (Y/n): " YESNO
 
 YESNO=${YESNO:-Y}
 
 if [[ $YESNO =~ ^[yY]$ ]]; then
-	echo "YES!!!"
+	echo "doing it..."
+	for f in $(ls $FILES_DIR); do
+		ln -vfs "$PWD/$FILES_DIR/$f" "$HOME/.$f"
+	done
 fi
 
-
-FILES_DIR="thefiles"
-
-for f in $(ls $FILES_DIR); do
-	echo $f
-done
 
