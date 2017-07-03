13123a56d8836030c4e5d6612b1ea31031672ebc
Amir Eldor <amir@eize.ninja>
2017-07-04 01:55:57 +0300

    git-prompt + add its config to bashrc script

---
+++ nicescripts/60_git-prompt.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+# Install and configure git prompt
+
+cd ~
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
+
+if ! grep -q GIT_PROMPT_ONLY_IN_REPO "$HOME/.bashrc"; then
+
+cat <<CONFIG >> ~/.bashrc
+
+
+# Set config variables first
+GIT_PROMPT_ONLY_IN_REPO=1
+
+# GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
+
+# GIT_PROMPT_SHOW_UPSTREAM=1 # uncomment to show upstream tracking branch
+# GIT_PROMPT_SHOW_UNTRACKED_FILES=all # can be no, normal or all; determines counting of untracked files
+
+# GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0 # uncomment to avoid printing the number of changed files
+
+# GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+
+# GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+# GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+
+# as last entry source the gitprompt script
+GIT_PROMPT_THEME=Evermeet # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+# GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
+# GIT_PROMPT_THEME=Solarized # use theme o
+source ~/.bash-git-prompt/gitprompt.sh
+CONFIG
+
+fi
